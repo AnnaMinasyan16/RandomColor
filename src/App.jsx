c46e5c3ccac274f3a5472d00364b1089466a17b7
AnnaMinasyan16 <minasyananna10@gmail.com>
2025-05-08 18:06:33 +0400

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
 import ColorPalette from "./components/ColorPalette";
 import UsersInput from "./components/UsersInput";
@@ -6,8 +6,9 @@ import UsersInput from "./components/UsersInput";
 function App() {
   return (
     <Routes>
+      {/* <Route path="/" element={<Navigate to="/RandomColor" replace />} /> */}
       <Route path="/ColorPalette" element={<ColorPalette />} />
-      <Route path="RandomColor/" element={<UsersInput />} />
+      <Route path="/RandomColor" element={<UsersInput />} />
     </Routes>
   );
 }
